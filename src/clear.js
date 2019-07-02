2a340de1e07281bdaa8846a4aa6c655adec1c887
Julian Rice <julianrice@dotcomjungle.com>
2019-07-01 21:33:54 -0700

    initial commit; working app with save, clear, load; copy and paste capable

---
+++ src/clear.js
@@ -0,0 +1,13 @@
+// EPIPHANY
+// v1
+// JULIAN RICE
+
+// Clears content
+
+function clear() {
+    document.getElementById("heading").innerText = "Title"; // heading div
+    document.getElementById("content").innerText = ""; // content div
+
+  }
+  
+clear();
